b695b052fb0e6c9c022c578b70948a5cabe80630
pmaan08 <partulmann@gmail.com>
2024-10-21 19:43:56 +0530

    Initial GrowWealth setup with Gradle

---
+++ src/main/java/com/wealth/growwealth/GrowWealthApplication.java
@@ -0,0 +1,13 @@
+package com.wealth.growwealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowWealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowWealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wealth/growwealth/GrowWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealth.growwealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowWealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
