5d9fdcb1097bb8628d0f5383e741110466c19886
pmaan08 <partulmann@gmail.com>
2024-11-21 03:46:16 +0530

    [SELF-15] implementing CORS bean in spring security config

--- src/main/java/com/wealth/growwealth/config/WebSecurityConfig.java
+++ src/main/java/com/wealth/growwealth/config/WebSecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,10 +24,20 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/clients","/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .cors(Customizer.withDefaults());
 
         return http.build();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") //
+                .allowCredentials(true);
+    }
 
 }
